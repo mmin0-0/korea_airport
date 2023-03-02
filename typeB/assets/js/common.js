554e081fe8eb049918ab9778d6341fa5583bd407
Hyunjin Bae <zmfhel77@gmail.com>
2023-03-02 17:51:12 +0900

    0302 / 17:50 / noah
    gnb 작업중

--- typeB/assets/js/common.js
+++ typeB/assets/js/common.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 
-    $('.ham-btn-wrap').on('click', function() {
-        console.log('123');
+    $(document).on('click', '.ham-btn-wrap', function() {
         $(this).toggleClass('active');
         if ($(this).hasClass('active')) {
-            $(this).parents('nav').addClass('active');
+            $(this).closest('.header-inner').find('.nav-wrap').addClass('active');
+            $(this).children('span').html('CLOSE');
+            $(this).siblings('.language').addClass('active');
         } else {
-            $(this).parents('nav').removeClass('active');
+            $(this).closest('.header-inner').find('.nav-wrap').removeClass('active');
+            $(this).children('span').html('MENU');
+            $(this).siblings('.language').removeClass('active');
         }
     });
 });
