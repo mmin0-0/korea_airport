9656f8c9c3f767193dc48097a870ad9f9e2723e0
Hyunjin Bae <101539396+goyangcityboy@users.noreply.github.com>
2023-03-09 04:14:55 +0900

    0309 / 16:14 / noah
    가로 스크롤 된ㄷ다...

--- typeB/assets/js/common.js
+++ typeB/assets/js/common.js
@@ -45,34 +45,61 @@ $(document).ready(function() {
     /* hero-wrap scroll trigger */
     gsap.registerPlugin(ScrollTrigger);
 
-    function heroWrap() {
-        const hero = $('#hero');
-        
-        gsap.to(hero, {
-            scrollTrigger: {
-                trigger: "#hero",
-                start: "top top", 
-                end: "bottom 0%",
-                pin: ".content",
-                markers: true
-            }
-        });
-    }
-    heroWrap();
-
-    // let sections = gsap.utils.toArray(".panel");
-
-    // gsap.to(sections, {
-    // xPercent: -100 * (sections.length - 1),
-    // ease: "none",
-    // scrollTrigger: {
-    //     trigger: "#heroSlide",
-    //     pin: true,
-    //     scrub: 1,
-    //     snap: 1 / (sections.length - 1),
-    //     end: () => "+=" + document.querySelector("#heroSlide").offsetWidth
-    // }
-    // });
+    const pageContainer = document.querySelector(".container");
+    
+    /* SMOOTH SCROLL */
+    const scroller = new LocomotiveScroll({
+      el: pageContainer,
+      smooth: true
+    });
+    
+    scroller.on("scroll", ScrollTrigger.update);
+    
+    ScrollTrigger.scrollerProxy(pageContainer, {
+      scrollTop(value) {
+        return arguments.length
+          ? scroller.scrollTo(value, 0, 0)
+          : scroller.scroll.instance.scroll.y;
+      },
+      getBoundingClientRect() {
+        return {
+          left: 0,
+          top: 0,
+          width: window.innerWidth,
+          height: window.innerHeight
+        };
+      },
+      pinType: pageContainer.style.transform ? "transform" : "fixed"
+    });
+    
+    ////////////////////////////////////
+    ////////////////////////////////////
+    window.addEventListener("load", function () {
+      let pinBoxes = document.querySelectorAll(".pin-wrap > *");
+      let pinWrap = document.querySelector(".pin-wrap");
+      let pinWrapWidth = pinWrap.offsetWidth;
+      let horizontalScrollLength = pinWrapWidth - window.innerWidth;
+    
+      // Pinning and horizontal scrolling
+    
+      gsap.to(".pin-wrap", {
+        scrollTrigger: {
+          scroller: pageContainer, //locomotive-scroll
+          scrub: true,
+          trigger: "#sectionPin",
+          pin: true,
+          // anticipatePin: 1,
+          start: "top top",
+          end: pinWrapWidth
+        },
+        x: -horizontalScrollLength,
+        ease: "none"
+      });
+    
+      ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
+    
+      ScrollTrigger.refresh();
+    });
 
     
 });
