f97d55b02a5ca7ee57abb457131b3789c92635d7
ABCJIIN <97517088+ABCJIIN@users.noreply.github.com>
2023-03-14 14:02:30 +0900

    0314 / 14:02 / jinny

---
+++ typeC/assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var mobileSwiper = new Swiper(".demo-slide", {
+        slidesPerView: 1,
+        // spaceBetween: 30,
+        // autoHeight : true,
+        loop: false
+    });
+});
